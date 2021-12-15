ed96dedbd2451713e56e65f156b4caa7f4aa021b
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-12-15 23:22:32 +0530

    Update fconvert.py

--- userbot/plugins/fconvert.py
+++ userbot/plugins/fconvert.py
@@ -5,6 +5,16 @@ from datetime import datetime
 from io import BytesIO
 from pathlib import Path
 
+import os
+from datetime import datetime
+
+import requests
+
+from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.cmdhelp import CmdHelp
+
+from . import *
+
 from telethon import functions, types
 from telethon.errors import PhotoInvalidDimensionsError
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -319,8 +329,167 @@ async def _(event):
             os.remove(new_required_file_name)
             await event.delete()
 
+            
+            
+            
+import os
+from datetime import datetime
 
-CmdHelp("fconvert").add_command(
+import requests
+
+from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.cmdhelp import CmdHelp
+
+from . import *
+
+
+@bot.on(admin_cmd(pattern=r"open", outgoing=True))
+async def _(event):
+    b = await event.client.download_media(await event.get_reply_message())
+    a = open(b, "r")
+    c = a.read()
+    a.close()
+    a = await event.reply("Reading file...")
+    if len(c) >= 4096:
+        await event.edit("output file too large lemme paste it 😜😜")  # hehe
+        out = c
+        url = "https://del.dog/documents"
+        r = requests.post(url, data=out.encode("UTF-8")).json()
+        url = f"https://del.dog/{r['key']}"
+        await event.edit(
+            f" Output file is too large Not supported By Telegram\n**So Pasted to** [Dog Bin]({url}) 😁😁",
+            link_preview=False,
+        )
+        await a.delete()
+    else:
+        await event.client.send_message(event.chat_id, f"{c}")
+        await a.delete()
+    os.remove(b)
+
+
+@bot.on(admin_cmd(pattern="doc ?(.*)"))
+async def get(event):
+    name = event.text[5:]
+    if name is None:
+        await event.edit("reply to text message as .ttf <file name>")
+        return
+    m = await event.get_reply_message()
+    if m.text:
+        with open(name, "w") as f:
+            f.write(m.message)
+        await event.delete()
+        await event.client.send_file(event.chat_id, name, force_document=True)
+        os.remove(name)
+    else:
+        await event.edit("reply to text message as .doc <file name.extension>")
+
+
+thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
+
+
+@bot.on(admin_cmd(pattern="stoi"))
+@bot.on(sudo_cmd(pattern="stoi", allow_sudo=True))
+async def danish(hehe):
+    if hehe.fwd_from:
+        return
+    thumb = None
+    hehe.message.id
+    if hehe.reply_to_msg_id:
+        hehe.reply_to_msg_id
+    cobra = await edit_or_reply(hehe, "Converting.....")
+
+    input_str = "dc.jpeg"
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if cobra.reply_to_msg_id:
+        datetime.datetime.now()
+        file_name = input_str
+        reply_message = await cobra.get_reply_message()
+
+        to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
+        downloaded_file_name = os.path.join(to_download_directory, file_name)
+        downloaded_file_name = await hehe.client.download_media(
+            reply_message, downloaded_file_name
+        )
+
+        try:
+            thumb = await reply_message.download_media(thumb=-1)
+        except Exception:
+            thumb = thumb
+        if os.path.exists(downloaded_file_name):
+
+            dc = await hehe.client.send_file(
+                hehe.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                supports_streaming=True,
+                allow_cache=False,
+                reply_to=reply_message,
+                thumb=thumb,
+            )
+
+            os.remove(downloaded_file_name)
+            await cobra.delete()
+        else:
+            await cobra.edit("Something went wrong")
+    else:
+        await cobra.edit("reply to a non animated sticker")
+
+
+# hehe
+
+
+@bot.on(admin_cmd(pattern="itos"))
+@bot.on(sudo_cmd(pattern="itos", allow_sudo=True))
+async def teamcobra(hehe):
+    if hehe.fwd_from:
+        return
+    thumb = None
+    hehe.message.id
+    if hehe.reply_to_msg_id:
+        hehe.reply_to_msg_id
+    cobra = await edit_or_reply(hehe, "Converting.....")
+
+    input_str = "dc.webp"
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    if cobra.reply_to_msg_id:
+        datetime.datetime.now()
+        file_name = input_str
+        reply_message = await cobra.get_reply_message()
+
+        to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
+        downloaded_file_name = os.path.join(to_download_directory, file_name)
+        downloaded_file_name = await hehe.client.download_media(
+            reply_message, downloaded_file_name
+        )
+
+        try:
+            thumb = await reply_message.download_media(thumb=-1)
+        except Exception:
+            thumb = thumb
+        if os.path.exists(downloaded_file_name):
+
+            dc = await hehe.client.send_file(
+                hehe.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                supports_streaming=True,
+                allow_cache=False,
+                reply_to=reply_message,
+                thumb=thumb,
+            )
+
+            os.remove(downloaded_file_name)
+            await cobra.delete()
+        else:
+            await cobra.edit("Something went wrong")
+    else:
+        await cobra.edit("reply to a non animated sticker")
+        
+        
+
+CmdHelp("fileconvert").add_command(
     "stim", "<reply to a sticker", "Converts the replied sticker into an image"
 ).add_command(
     "itom", "<reply to a image>", "Converts the replied image to sticker"
@@ -342,4 +511,12 @@ CmdHelp("fconvert").add_command(
     "nfc mp3",
     "<reply to media to extract mp3>",
     "Converts the replied media file to mp3",
+).add_command("open", None, "open files as text "
+).add_command(
+    ".doc <file name.extension> <reply to any text/media>",
+    None,
+    "Create a document of anything (example:- .doc dc.mp4, .doc dc.txt, .doc dc.webp)",
+).add_command("stoi", None, "★  Convert sticker to image"
+).add_command(
+    "itos", None, "Convert Image to Sticker"
 ).add()
