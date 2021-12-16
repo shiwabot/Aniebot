cd6bec892386aa05432e1565194165b1e3c70bd0
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-12-16 07:11:49 +0530

    Update fileconvert.py

--- userbot/helpers/utils/fileconvert.py
+++ userbot/helpers/utils/fileconvert.py
@@ -165,3 +165,59 @@ async def unsavegif(event, sandy):
         )
     except Exception as e:
         LOGS.info(str(e))
+
+
+async def edit_or_freply(
+    event,
+    text,
+    parse_mode=None,
+    link_preview=None,
+    file_name=None,
+    aslink=False,
+    deflink=False,
+    noformat=False,
+    linktext=None,
+    caption=None,
+):  # sourcery no-metrics
+    sudo_users = _sudousers_list()
+    link_preview = link_preview or False
+    reply_to = await event.get_reply_message()
+    if len(text) < 4096 and not deflink:
+        parse_mode = parse_mode or "md"
+        if event.sender_id in sudo_users:
+            if reply_to:
+                return await reply_to.reply(
+                    text, parse_mode=parse_mode, link_preview=link_preview
+                )
+            return await event.reply(
+                text, parse_mode=parse_mode, link_preview=link_preview
+            )
+        await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
+        return event
+    if not noformat:
+        text = md_to_text(text)
+    if aslink or deflink:
+        linktext = linktext or "Message was to big so pasted to bin"
+        response = await paste_message(text, pastetype="s")
+        text = linktext + f" [here]({response})"
+        if event.sender_id in sudo_users:
+            if reply_to:
+                return await reply_to.reply(text, link_preview=link_preview)
+            return await event.reply(text, link_preview=link_preview)
+        await event.edit(text, link_preview=link_preview)
+        return event
+    file_name = file_name or "output.txt"
+    caption = caption or None
+    with open(file_name, "w+") as output:
+        output.write(text)
+    if reply_to:
+        await reply_to.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    if event.sender_id in sudo_users:
+        await event.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    await event.client.send_file(event.chat_id, file_name, caption=caption)
+    await event.delete()
+    os.remove(file_name)
