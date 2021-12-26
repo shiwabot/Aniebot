92703889b1be8b95f975ccb8f08ee0cfc901a820
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-12-26 15:50:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,11 +25,11 @@ load_dotenv()
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
+        self.API_ID: str = os.environ.get("APP_ID", None)
         self.API_HASH: str = os.environ.get("API_HASH", None)
         self.SESSION: str = os.environ.get("SESSION", None)
         self.SUDOERS: list = [
-            int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
+            int(id) for id in os.environ.get("SUDOUP", " ").split() if id.isnumeric()
         ]
         if not self.SESSION or not self.API_ID or not self.API_HASH:
             print("Error: SESSION, API_ID and API_HASH is required!")
