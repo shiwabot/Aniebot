25637f4e46828b243209ad3d9878acb6ad9991e9
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2022-01-06 03:59:53 +0530

    Create png.py

---
+++ userbot/plugins/png.py
@@ -0,0 +1,118 @@
+import io
+import math
+import random
+import urllib.request
+from os import remove
+
+from PIL import Image
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import (
+    DocumentAttributeFilename,
+    DocumentAttributeSticker,
+    InputStickerSetID,
+    MessageMediaPhoto,
+)
+
+from userbot.cmdhelp import CmdHelp
+from userbot.utils import *
+
+from . import *
+
+KANGING_STR = [
+    "Using Witchery to kang this sticker...",
+    "Plagiarising hehe...",
+    "Inviting this sticker over to my pack...",
+    "Kanging this sticker...",
+    "Hey that's a nice sticker!\nMind if I kang?!..",
+    "hehe me stel ur stikér\nhehe.",
+    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
+    "Roses are red violets are blue, kanging this sticker so my pacc looks cool",
+    "Imprisoning this sticker...",
+    "Mr.Steal Your Sticker is stealing this sticker... ",
+]
+
+legend = Config.CUSTOM_STICKER_PACK_NAME
+
+@bot.on(admin_cmd(outgoing=True, pattern="kang"))
+@bot.on(sudo_cmd(pattern="kang", allow_sudo=True))
+async def kang(args):
+    """For .kang command, kangs stickers or creates new ones."""
+    user = await bot.get_me()
+    if not user.username:
+        user.username = user.first_name
+    message = await args.get_reply_message()
+    photo = None
+    emojibypass = False
+    is_anim = False
+    emoji = None
+
+    if message and message.media:
+        if isinstance(message.media, MessageMediaPhoto):
+            await args.edit(f"`{random.choice(KANGING_STR)}`")
+            photo = io.BytesIO()
+            photo = await bot.download_media(message.photo, photo)
+        elif "image" in message.media.document.mime_type.split("/"):
+            await args.edit(f"`{random.choice(KANGING_STR)}`")
+            photo = io.BytesIO()
+            await bot.download_file(message.media.document, photo)
+            if (
+                DocumentAttributeFilename(file_name="sticker.webp")
+                in message.media.document.attributes
+            ):
+                emoji = message.media.document.attributes[1].alt
+                emojibypass = True
+        elif "tgsticker" in message.media.document.mime_type:
+            await args.edit(f"`{random.choice(KANGING_STR)}`")
+            await bot.download_file(message.media.document, "AnimatedSticker.tgs")
+
+            attributes = message.media.document.attributes
+            for attribute in attributes:
+                if isinstance(attribute, DocumentAttributeSticker):
+                    emoji = attribute.alt
+
+            emojibypass = True
+            is_anim = True
+            photo = 1
+        else:
+            await args.edit("`Unsupported File!`")
+            return
+    else:
+        await args.edit("`I can't kang that...`")
+        return
+
+    if photo:
+        splat = args.text.split()
+        if not emojibypass:
+            emoji = "😎"
+        pack = 1
+        if len(splat) == 3:
+            pack = splat[2]  # User sent both
+            emoji = splat[1]
+        elif len(splat) == 2:
+            if splat[1].isnumeric():
+                # User wants to push into different pack, but is okay with
+                # thonk as emote.
+                pack = int(splat[1])
+            else:
+                # User sent just custom emote, wants to push to default
+                # pack
+                emoji = splat[1]
+
+        packname = f"{user.username}"
+        packnick = (
+            f"{legend} Vol.{pack}"
+            if legend
+            else f"@{user.username}'s legend Vol.{pack}"
+        )
+        cmd = "/newpack"
+        file = io.BytesIO()
+
+        if not is_anim:
+            image = await resize_photo(photo)
+            file.name = "sticker.png"
+            image.save(file, "PNG")
+        else:
+            packname += "_anim"
+            packnick += " (Animated)"
+            cmd = "/newanimated"
+        await bot.send_file(args.chat_id, packnick, caption=f"Hello")
