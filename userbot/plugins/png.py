2a984dabf397efd82b16429aff534670548d07b5
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2022-01-06 04:34:59 +0530

    Update png.py

--- userbot/plugins/png.py
+++ userbot/plugins/png.py
@@ -1,5 +1,7 @@
+
 import io
 import math
+import random
 
 from PIL import Image
 from telethon.tl.types import (
@@ -12,25 +14,41 @@ from userbot.utils import *
 
 from . import *
 
+KANGING_STR = [
+    "Using Witchery to kang this sticker...",
+    "Plagiarising hehe...",
+    "Inviting this sticker over to my pack...",
+    "Kanging this sticker...",
+    "Hey that's a nice sticker!\nMind if I kang?!..",
+    "hehe me stel ur stikér\nhehe.",
+    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
+    "Roses are red violets are blue, kanging this sticker so my pacc looks cool",
+    "Imprisoning this sticker...",
+    "Mr.Steal Your Sticker is stealing this sticker... ",
+]
+
 legend = Config.CUSTOM_STICKER_PACK_NAME
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="png"))
 @bot.on(sudo_cmd(pattern="png", allow_sudo=True))
 async def kang(args):
+    """For .kang command, kangs stickers or creates new ones."""
     user = await bot.get_me()
     if not user.username:
         user.username = user.first_name
     message = await args.get_reply_message()
     photo = None
+    emojibypass = False
     is_anim = False
 
     if message and message.media:
         if isinstance(message.media, MessageMediaPhoto):
+            await args.edit(f"`{random.choice(KANGING_STR)}`")
             photo = io.BytesIO()
             photo = await bot.download_media(message.photo, photo)
         elif "image" in message.media.document.mime_type.split("/"):
-            await args.edit(f"`Wait A Sec Its On Process`")
+            await args.edit(f"`{random.choice(KANGING_STR)}`")
             photo = io.BytesIO()
             await bot.download_file(message.media.document, photo)
             if (
@@ -38,7 +56,9 @@ async def kang(args):
                 in message.media.document.attributes
             ):
                 message.media.document.attributes[1].alt
+                emojibypass = True
         elif "tgsticker" in message.media.document.mime_type:
+            await args.edit(f"`{random.choice(KANGING_STR)}`")
             await bot.download_file(message.media.document, "AnimatedSticker.tgs")
 
             attributes = message.media.document.attributes
@@ -46,15 +66,40 @@ async def kang(args):
                 if isinstance(attribute, DocumentAttributeSticker):
                     attribute.alt
 
+            emojibypass = True
             is_anim = True
             photo = 1
         else:
             await args.edit("`Unsupported File!`")
             return
     else:
-        await args.edit("`No Check One More time`")
+        await args.edit("`I can't kang that...`")
         return
 
+    if photo:
+        splat = args.text.split()
+        if not emojibypass:
+            pass
+        pack = 1
+        if len(splat) == 3:
+            pack = splat[2]  # User sent both
+            splat[1]
+        elif len(splat) == 2:
+            if splat[1].isnumeric():
+                # User wants to push into different pack, but is okay with
+                # thonk as emote.
+                pack = int(splat[1])
+            else:
+                # User sent just custom emote, wants to push to default
+                # pack
+                splat[1]
+
+        packname = f"{user.username}"
+        packnick = (
+            f"{legend} Vol.{pack}"
+            if legend
+            else f"@{user.username}'s legend Vol.{pack}"
+        )
         file = io.BytesIO()
 
         if not is_anim:
@@ -62,14 +107,15 @@ async def kang(args):
             file.name = "sticker.png"
             image.save(file, "PNG")
         else:
-            pass
+            packname += "_anim"
+            packnick += " (Animated)"
         if is_anim:
             await bot.send_file(arg.chat_id, "AnimatedSticker.tgs")
             remove(args.chat_id, "AnimatedSticker.tgs")
         else:
             file.seek(0)
             await bot.send_file(args.chat_id, file, force_document=True)
-    #  await bot.send_file(args.chat_id, packnick, caption=f"Hello")
+      #  await bot.send_file(args.chat_id, packnick, caption=f"Hello")
 
 
 async def resize_photo(photo):
