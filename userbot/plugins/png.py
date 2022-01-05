76ceed9cd964361e09c10a7375ebda80e7785612
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2022-01-06 04:11:06 +0530

    Update png.py

--- userbot/plugins/png.py
+++ userbot/plugins/png.py
@@ -6,7 +6,25 @@ from telethon.tl.types import (
     DocumentAttributeSticker,
     MessageMediaPhoto,
 )
+import io
+import math
+import random
+import urllib.request
+from os import remove
 
+from PIL import Image
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import (
+    DocumentAttributeFilename,
+    DocumentAttributeSticker,
+    InputStickerSetID,
+    MessageMediaPhoto,
+)
+
+from userbot.cmdhelp import CmdHelp
+from userbot.utils import *
+
+from . import *
 from userbot.utils import *
 
 from . import *
@@ -27,8 +45,8 @@ KANGING_STR = [
 legend = Config.CUSTOM_STICKER_PACK_NAME
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="kang"))
-@bot.on(sudo_cmd(pattern="kang", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="png"))
+@bot.on(sudo_cmd(pattern="png", allow_sudo=True))
 async def kang(args):
     """For .kang command, kangs stickers or creates new ones."""
     user = await bot.get_me()
@@ -107,3 +125,30 @@ async def kang(args):
             packname += "_anim"
             packnick += " (Animated)"
         await bot.send_file(args.chat_id, packnick, caption=f"Hello")
+
+
+
+async def resize_photo(photo):
+    """Resize the given photo to 512x512"""
+    image = Image.open(photo)
+    maxsize = (512, 512)
+    if (image.width and image.height) < 512:
+        size1 = image.width
+        size2 = image.height
+        if image.width > image.height:
+            scale = 512 / size1
+            size1new = 512
+            size2new = size2 * scale
+        else:
+            scale = 512 / size2
+            size1new = size1 * scale
+            size2new = 512
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        image = image.resize(sizenew)
+    else:
+        image.thumbnail(maxsize)
+
+    return image
+
