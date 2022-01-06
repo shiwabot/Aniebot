22b2bf760175d934c18adfd3ef29ce3778a0beb2
LEGEND-OS <Krishna045Jaiswal@gmail.com>
2022-01-06 18:09:03 +0000

    auto fixes

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -109,9 +109,7 @@ async def killer():
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_file(
-                "@BotFather", "userbot/resources/pics/main.jpg"
-            )
+            await bot.send_file("@BotFather", "userbot/resources/pics/main.jpg")
             await asyncio.sleep(2)
         except Exception as e:
             print(e)
