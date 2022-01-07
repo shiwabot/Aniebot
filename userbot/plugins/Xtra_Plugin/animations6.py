e577e0d312ed80a8a82d79a44b7afcf5b3f5676e
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2022-01-08 03:05:21 +0530

    Update animations6.py

--- userbot/plugins/Xtra_Plugin/animations6.py
+++ userbot/plugins/Xtra_Plugin/animations6.py
@@ -436,6 +436,33 @@ async def _(event):
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 35])
 
+@bot.on(admin_cmd(pattern="yaadash"))
+async def _(event):
+    await event.edit("***falled while walking***")
+    await asyncio.sleep(3)
+    await event.edit("***2 Hours Later***")
+    await asyncio.sleep(6)
+    await event.edit("who i am here ?")
+    await asyncio.sleep(1)
+    await event.edit("Where I Am?")
+    await asyncio.sleep(1)
+    await event.edit("What I Am Using?")
+    await asyncio.sleep(1)
+    await event.edit("What Is Phone?")
+    await asyncio.sleep(1)
+    await event.edit("What Is Telegram?")
+    await asyncio.sleep(1)
+    await event.edit("Why Life Exists?")
+    await asyncio.sleep(1)
+    await event.edit("What is Language?")
+    await asyncio.sleep(1)
+    await event.edit("Does Reborn Take Place?")
+    await asyncio.sleep(1)
+    await event.edit("What Is Thinking?")
+    await asyncio.sleep(1)
+    await event.edit("Does Superman Exists")
+    await asyncio.sleep(3)
+    await event.edit("Oof I Guess I Should Die")
 
 CmdHelp("animations6").add_command("bigoof", None, "🇮🇳🇮🇳🇮🇳").add_command(
     "imhappy", None, "Use and see"
@@ -455,6 +482,8 @@ CmdHelp("animations6").add_command("bigoof", None, "🇮🇳🇮🇳🇮🇳").a
     "sadmin", None, "Its Shout Admin"
 ).add_command(
     "friend", None, "Use and See"
+).add_command(
+    "yaadash", None, "Use And See"
 ).add_info(
     "Its all Animation Use As Anywhere"
 ).add_warning(
