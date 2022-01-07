3377c9cb63ca3c1374c5232c6ed9d5eb84125de7
LEGEND-OS <Krishna045Jaiswal@gmail.com>
2022-01-07 20:25:48 +0000

    auto fixes

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -1,7 +1,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2
-from telethon.tl.types import ChannelParticipantsAdmins
 
 from LEGENDBOT.utils import admin_cmd, sudo_cmd
 from userbot.cmdhelp import CmdHelp
