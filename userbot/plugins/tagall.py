e0717fa6e24ae3cb525d5e4d4345539ec17c936c
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2022-01-08 01:55:08 +0530

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -13,14 +13,30 @@ async def _(event):
     if event.fwd_from:
         return
     mentions = event.pattern_match.group(1)
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, 100):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    await event.reply(mentions)
+    if mentions:
+        chat = await event.get_input_chat()
+        async for x in borg.iter_participants(chat, 200):
+            mentions += f" \n\n [{x.first_name}](tg://user?id={x.id})"
+        reply_message = None
+        if event.reply_to_msg_id:
+            reply_message = await event.get_reply_message()
+            await reply_message.reply(mentions)
+        else:
+            await event.reply(mentions)
+    else:
+        chat = await event.get_input_chat()
+        async for y in borg.iter_participants(chat, 100):
+            hello = f"Hi, How Are U\n\n [{y.first_name}](tg://user?id={y.id})"
+        reply_message = None
+        if event.reply_to_msg_id:
+            reply_message = await event.get_reply_message()
+            await reply_message.reply(hello)
+        else:
+            await event.reply(hello)
     await event.delete()
 
 
-@bot.on(admin_cmd(pattern=r"admins", outgoing=True))
+"""@bot.on(admin_cmd(pattern=r"admins", outgoing=True))
 @bot.on(sudo_cmd(pattern=r"admins", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
@@ -36,8 +52,8 @@ async def _(event):
     else:
         await event.reply(mentions)
     await event.delete()
-
+"""
 
 CmdHelp("tags").add_command(
     "tagall", "<text>", "Tags all the members in the group. (Max 100)"
-).add_command("admins", None, "Tags all the admins in the group").add()
+).add_command("admin", None, "Tags all the admins in the group").add()
