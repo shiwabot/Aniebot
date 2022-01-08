d14bc1b455a0cac53bc54087e8f565597d1449fa
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2022-01-08 12:03:44 +0530

    Update and rename design.py to joiner.py

--- userbot/plugins/joiner.py
+++ userbot/plugins/joiner.py
@@ -3,8 +3,14 @@ from telethon.tl.types import ChannelParticipantsAdmins
 
 from userbot.cmdhelp import CmdHelp
 from userbot.utils import admin_cmd
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import (
+    ChannelInvalidError,
+    ChannelPrivateError,
+    ChannelPublicGroupNaError,
+)
 
-CmdHelp("design").add_command("join", None, "use and see").add_command(
+CmdHelp("join").add_command("join", None, "use and see").add_command(
     "pay", None, "use and see"
 ).add()
 
@@ -13,18 +19,15 @@ CmdHelp("design").add_command("join", None, "use and see").add_command(
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "`━━━━━┓ \n┓┓┓┓┓┃\n┓┓┓┓┓┃　ヽ○ノ ⇦ Me When You Joined \n┓┓┓┓┓┃.     /　 \n┓┓┓┓┓┃ ノ) \n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃`"
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f""
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
-
+    yukki = event.pattern_match.group(1)
+    text = "Joining..."
+    event = await e.reply(text, parse_mode=None, link_preview=None )
+    try:
+        await e.client(functions.channels.JoinChannelRequest(channel=yukki))
+        await event.edit("𝐉𝐎𝐢𝐍 𝐇𝐎𝐆𝐘𝐀 𝐕𝐀𝐈 ")
+    except Exception as e:
+        await event.edit(str(e))   
+  
 
 @borg.on(admin_cmd("pay"))
 async def _(event):
