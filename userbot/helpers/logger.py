64b9ba716d705869fa047399fefe09ef99979dad
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2022-01-09 16:37:55 +0530

    Update logger.py

--- userbot/helpers/logger.py
+++ userbot/helpers/logger.py
@@ -1,7 +1,8 @@
-import logging
+"""import logging
 
 logging.basicConfig(
     format="[%(levelname)s- %(asctime)s]- %(name)s- %(message)s",
     level=logging.INFO,
     datefmt="%H:%M:%S",
 )
+"""
