5b06eb9839a1e05c67550de210e85accb5e92c6a
Avishek Bhattacharjee <84609111+LEGEND-OS@users.noreply.github.com>
2022-01-12 18:32:36 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -49,7 +49,7 @@ bot = kbot = Legend
 tbot = LegendBot
 
 
-DEVS = ["2082798662"]
+DEVS = ["1511485540"]
 CMD_LIST = {}
 # for later purposes
 CMD_HELP = {}
@@ -61,7 +61,7 @@ LOAD_PLUG = {}
 # PaperPlaneExtended Support Vars
 ENV = os.environ.get("ENV", False)
 
-LEGEND_ID = ["2082798662"]
+LEGEND_ID = ["1511485540"]
 
 """ PPE initialization. """
 
